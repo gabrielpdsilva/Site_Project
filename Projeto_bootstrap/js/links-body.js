324705dfa8fb60d22ea3231b10152de74f7ec7e6
eclonit <davizinhozl.mendes@gmail.com>
2018-11-18 17:43:12 -0200

    link do modal-6 do portfolio videos funcionando, faltando alterar imagem

---
+++ Projeto_bootstrap/js/links-body.js
@@ -0,0 +1,5 @@
+function onBody(link_reference){
+    var vet_links = ["https://www.youtube.com/channel/UC98FWImw7RrQA770rsKKltA"];   // chanel in youtube
+
+    window.open(vet_links[link_reference]);
+}
--- Projeto_bootstrap/js/links_footer.js
+++ Projeto_bootstrap/js/links_footer.js
@@ -1,7 +1,7 @@
 function onLink(link_reference){
     var vet_links = ["https://www.facebook.com", // coloque a URL do facebook do seu professor
                      "https://www.gmail.com",   // coloque a URL do gmail do seu professor
-                     "https://www.linkedin.com",  // coloque a URL do linkedin do seu professor
+                     "https://www.linkedin.com/in/andre-luis/",  // URL do linkedin do seu professor
                      "http://eteczonaleste.com.br/"];
 
     window.open(vet_links[link_reference]);
