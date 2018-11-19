6a9d15d1520f1f9e4a1624ac70ba42f0b9c9abc6
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2018-11-19 16:13:41 -0200

    rede social google plus adicionado no rodape

--- Projeto_bootstrap/js/links_footer.js
+++ Projeto_bootstrap/js/links_footer.js
@@ -1,6 +1,6 @@
 function onLink(link_reference){
     var vet_links = ["https://www.facebook.com", // coloque a URL do facebook do seu professor
-                     "https://www.gmail.com",   // coloque a URL do gmail do seu professor
+                     "https://plus.google.com/u/0/108821087658774461616",   // URL do google+ do professor
                      "https://www.linkedin.com/in/andre-luis/",  // URL do linkedin do seu professor
                      "http://eteczonaleste.com.br/"];
 
